2d0fe3ab864fa3d103ed351a304008ec6508af53
milusca_judith <miluscavega15@gmail.com>
2020-07-15 11:03:39 -0500

    agrega babelrc eslintrc src/index.html src/index.js y src/style.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
 
+const botonuno = document.getElementById('botonuno'); 
+    botonuno.addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+        if (email === '') {
+            document.getElementById('email').focus();
+        } else {
+        document.getElementById('email').focus();
+        document.getElementById('primerapag').style.display='none';
+        document.getElementById('segundapag').style.display='block';
+        }
+});
+
 console.log(validator);
