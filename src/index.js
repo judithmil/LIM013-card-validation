ddf8eb0e93775860532238209c1f679869a8343f
milusca_judith <miluscavega15@gmail.com>
2020-07-18 20:22:45 -0500

    agrega src/index.html src/index.js y src/style.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-const botonuno = document.getElementById('botonuno'); 
-    botonuno.addEventListener('click', () => {
-    const email = document.getElementById('email').value;
-        if (email === '') {
+//funcion primer boton-------------------------
+const boton1 = document.getElementById('boton1'); 
+    boton1.addEventListener('click', () => {
+    const correo = document.getElementById('email').value;
+        if (correo === "") {
             document.getElementById('email').focus();
         } else {
         document.getElementById('email').focus();
@@ -12,4 +13,28 @@ const botonuno = document.getElementById('botonuno');
         }
 });
 
-console.log(validator);
+//pantalla dos -------------------
+const boton2= document.getElementById('boton2');
+      boton2.addEventListener('click',() => {
+
+      const creditCardNumber= document.getElementById('creditCardNumber').value;
+      const nameClient= document.getElementById('nameClient').value;
+      const exp= document.getElementById('exp').value;
+      const cvv=document.getElementById('cvv').value;
+      if (nameClient === "" && creditCardNumber === "" && exp === "" && cvv === "") {
+           document.getElementById('nota1').innerHTML= 'Ingresar datos requeridos';
+      }  else {
+      const numeroTarjeta = document.getElementById('creditCardNumber');
+      const isValidCardNumber = validator.isValid(numeroTarjeta.value);
+
+
+
+
+
+      }
+      
+
+      });
+
+
+ //console.log(validator);
